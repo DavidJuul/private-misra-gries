31615fdeb57edb6ca216561772270ada2a29d235
David JT <djt@hotmail.dk>
2025-03-02 23:44:21 +0100

    Add optimizations for logarithmic time updates

--- pmg.py
+++ pmg.py
@@ -6,29 +6,77 @@ import secrets
 import sys
 
 from diffprivlib.mechanisms import Geometric
+from sortedcontainers import SortedList
 
 
 def misra_gries(k, stream):
-    sketch = {key: 0 for key in range(-1, -k - 1, -1)}
+    first_group = SketchGroup(0)
+    first_group.elements = SortedList(range(-k, 0))
+    sketch = {key: SketchElement(first_group) for key in range(-k, 0)}
 
-    for element in stream:
-        if element in sketch:
-            sketch[element] += 1
-        elif all(map(lambda x: sketch[x] >= 1, sketch)):
-            for key in sketch:
-                sketch[key] -= 1
+    for key in stream:
+        if key in sketch:
+            element = sketch[key]
+            if len(element.group.elements) == 1:
+                element.group.count_diff += 1
+            else:
+                element.group.elements.remove(key)
+                if len(element.group.elements) == 0:
+                    if element.group.prev:
+                        element.group.prev.next = element.group.next
+                    if element.group.next:
+                        element.group.next.prev = element.group.prev
+                if element.group.next and element.group.next.count_diff == 1:
+                    element.group = element.group.next
+                else:
+                    new_group = SketchGroup(1)
+                    if len(element.group.elements) == 0:
+                        new_group.prev = element.group.prev
+                    else:
+                        new_group.prev = element.group
+                    if element.group.next:
+                        new_group.next = element.group.next
+                        element.group.next.prev = new_group
+                    element.group.next = new_group
+                    element.group = new_group
+                if len(first_group.elements) == 0:
+                    first_group = first_group.next
+                element.group.elements.add(key)
+        elif first_group.count_diff >= 1:
+            first_group.count_diff -= 1
         else:
-            for key in sketch:
-                if sketch[key] == 0:
-                    break
-            del sketch[key]
-            sketch[element] = 1
+            min_zero_key = first_group.elements.pop(0)
+            del sketch[min_zero_key]
+            element = SketchElement()
+            sketch[key] = element
+            if first_group.next and first_group.next.count_diff == 1:
+                element.group = first_group.next
+            else:
+                new_group = SketchGroup(1)
+                new_group.prev = first_group
+                if first_group.next:
+                    new_group.next = first_group.next
+                    first_group.next.prev = new_group
+                first_group.next = new_group
+                element.group = new_group
+            element.group.elements.add(key)
+            if len(first_group.elements) == 0:
+                first_group = first_group.next
 
-    for key in sketch:
-        if key < 0:
-            del sketch[key]
+    group = first_group
+    count = 0
+    while group:
+        count += group.count_diff
+        for key in group.elements:
+            sketch[key] = count
+        group = group.next
 
-    return sketch
+    final_sketch = {}
+    for key in sorted(sketch):
+        if key >= 0:
+            final_sketch[key] = sketch[key]
+
+    return final_sketch
 
 
 def private_misra_gries(sketch, epsilon, delta):
@@ -46,6 +94,19 @@ def private_misra_gries(sketch, epsilon, delta):
             private_sketch[key] = counter
 
     return private_sketch
+
+
+class SketchElement:
+    def __init__(self, group=None):
+        self.group = group
+
+
+class SketchGroup:
+    def __init__(self, count_diff):
+        self.count_diff = count_diff
+        self.elements = SortedList()
+        self.prev = None
+        self.next = None
 
 
 def main():
