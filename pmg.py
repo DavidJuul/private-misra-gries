f5bd81ac0f86ad5ac4da78e5bf7c55ecb6c1146b
David JT <djt@hotmail.dk>
2025-04-03 21:46:01 +0200

    Clean up code

--- pmg.py
+++ pmg.py
@@ -12,9 +12,9 @@ from numpy.random import binomial
 RANDOM = random.SystemRandom()
 
 
-def misra_gries(stream, k):
-    sketch = {key: 0 for key in range(-k, 0)}
-    zero_group = list(range(-k, 0))
+def misra_gries(stream, sketch_size):
+    sketch = {key: 0 for key in range(-sketch_size, 0)}
+    zero_group = list(range(-sketch_size, 0))
     zero_pointer = 0
     element_count = 0
     decrement_count = 0
@@ -30,7 +30,7 @@ def misra_gries(stream, k):
         zero_group.sort()
         zero_pointer = 0
 
-    def insert_element(element):
+    def insert_key(key):
         nonlocal zero_pointer
         while True:
             removed_key = zero_group[zero_pointer]
@@ -41,7 +41,7 @@ def misra_gries(stream, k):
                 decrement_all()
                 return
         del sketch[removed_key]
-        sketch[element] = 1
+        sketch[key] = 1
 
     for element in stream:
         element_count += 1
@@ -50,7 +50,7 @@ def misra_gries(stream, k):
         elif zero_pointer == len(zero_group):
             decrement_all()
         else:
-            insert_element(element)
+            insert_key(element)
 
     final_sketch = {}
     for key in sorted(sketch):
@@ -60,8 +60,8 @@ def misra_gries(stream, k):
     return final_sketch, element_count, decrement_count
 
 
-def private_misra_gries(sketch, epsilon, delta, sensitivity=1, threshold=-1,
-                        add_global_noise=True):
+def privatize_misra_gries(sketch, epsilon, delta, sensitivity=1, threshold=-1,
+                          add_global_noise=True):
     if threshold == -1:
         threshold = math.ceil(
             1 + 2 * (math.log(6 * math.exp(epsilon)
@@ -79,13 +79,14 @@ def private_misra_gries(sketch, epsilon, delta, sensitivity=1, threshold=-1,
     return private_sketch
 
 
-def pure_private_misra_gries(sketch, k, epsilon, universe_size, element_count,
-                             decrement_count, sensitivity=2,
-                             offset_counts=True):
-    offset = (decrement_count - math.floor(element_count / (k + 1))
+def purely_privatize_misra_gries(sketch, sketch_size, epsilon, universe_size,
+                                 element_count, decrement_count, sensitivity=2,
+                                 offset_counts=True):
+    offset = (decrement_count - math.floor(element_count / (sketch_size + 1))
               if offset_counts else 0)
     threshold = math.ceil(
-        math.log((1 + math.exp(-epsilon / sensitivity)) * k / universe_size)
+        math.log((1 + math.exp(-epsilon / sensitivity)) * sketch_size
+                 / universe_size)
         / math.log(math.exp(-epsilon / sensitivity)))
     geometric = create_geometric(epsilon, sensitivity)
     two_sided_geometric = create_two_sided_geometric(epsilon, sensitivity)
@@ -96,22 +97,22 @@ def pure_private_misra_gries(sketch, k, epsilon, universe_size, element_count,
         if counter >= threshold:
             noisy_sketch[key] = counter
 
-    upgrade_count = binomial(universe_size, k / universe_size)
+    upgrade_count = binomial(universe_size, sketch_size / universe_size)
     while upgrade_count > 0:
         key = RANDOM.randrange(0, universe_size)
         if key not in noisy_sketch:
             noisy_sketch[key] = threshold + geometric()
             upgrade_count -= 1
 
-    top_k = sorted(noisy_sketch.items(), key=lambda item: item[1])[-k:]
+    top_k = sorted(noisy_sketch.items(),
+                   key=lambda item: item[1])[-sketch_size:]
     private_sketch = dict(sorted(top_k))
 
     return private_sketch
 
 
-def merge(sketches, k):
+def merge(sketches, sketch_size):
     merged = sketches[0]
-
     for sketch in sketches[1:]:
         summed_sketch = merged
         for key in sketch:
@@ -120,29 +121,33 @@ def merge(sketches, k):
             else:
                 summed_sketch[key] = sketch[key]
 
-        if len(summed_sketch) > k:
+        if len(summed_sketch) > sketch_size:
             offset = sorted(summed_sketch.items(),
-                            key=lambda item: item[1])[-(k + 1)][1]
+                            key=lambda item: item[1])[-(sketch_size + 1)][1]
+            for key in summed_sketch:
+                if summed_sketch[key] > offset:
+                    merged[key] = summed_sketch[key] - offset
         else:
-            offset = 0
-        for key in summed_sketch:
-            if summed_sketch[key] > offset:
-                merged[key] = summed_sketch[key] - offset
+            merged = dict(summed_sketch)
 
     return merged
 
 
-def private_merge(merged, k, epsilon, delta):
+def privatize_merged(merged, sketch_size, epsilon, delta):
     threshold = math.ceil(
-        1 + 2 * (k * math.log(2 * k * math.exp(epsilon / k)
-                              / ((math.exp(epsilon / k) + 1) * delta))
+        1 + 2 * (sketch_size * math.log(2 * sketch_size
+                                        * math.exp(epsilon / sketch_size)
+                              / ((math.exp(epsilon / sketch_size) + 1)
+                                 * delta))
                  / epsilon))
-    return private_misra_gries(merged, epsilon, delta, k, threshold, False)
+    return privatize_misra_gries(merged, epsilon, delta, sketch_size,
+                                 threshold, False)
 
 
-def pure_private_merge(merged, k, epsilon, universe_size):
-    return pure_private_misra_gries(merged, k, epsilon, universe_size, None,
-                                    None, k, False)
+def purely_privatize_merged(merged, sketch_size, epsilon, universe_size):
+    return purely_privatize_misra_gries(merged, sketch_size, epsilon,
+                                        universe_size, None, None, sketch_size,
+                                        False)
 
 
 def create_geometric(epsilon, sensitivity):
@@ -153,6 +158,69 @@ def create_geometric(epsilon, sensitivity):
 def create_two_sided_geometric(epsilon, sensitivity):
     geometric = create_geometric(epsilon, sensitivity)
     return lambda: geometric() - geometric()
+
+
+def create_sketch():
+    sketch_size = int(sys.argv[1])
+    epsilon = float(sys.argv[2])
+    delta = float(sys.argv[3])
+    if delta > 0:
+        file = sys.argv[4]
+    else:
+        universe_size = int(sys.argv[4])
+        file = sys.argv[5]
+
+    # Create the sketch from the file stream.
+    with open(file, encoding="utf8") as stream:
+        stream = map(int, stream)
+        sketch, element_count, decrement_count = misra_gries(stream,
+                                                             sketch_size)
+
+    # Privatize the sketch.
+    if delta > 0:
+        private_sketch = privatize_misra_gries(sketch, epsilon, delta)
+    else:
+        private_sketch = purely_privatize_misra_gries(
+            sketch, sketch_size, epsilon, element_count, decrement_count,
+            universe_size)
+
+    # Output the sketch.
+    print("Sketch        :", sketch)
+    print("Private sketch:", private_sketch)
+    if ((delta > 0 and len(sys.argv) >= 6) or len(sys.argv) >= 7):
+        output_file = sys.argv[5] if delta > 0 else sys.argv[6]
+        with open(output_file, "w", encoding="utf8") as output:
+            json.dump(sketch, output)
+
+
+def merge_sketches():
+    sketch_size = int(sys.argv[2])
+    epsilon = float(sys.argv[3])
+    delta = float(sys.argv[4])
+    if delta > 0:
+        sketch_files = sys.argv[5:]
+    else:
+        universe_size = int(sys.argv[5])
+        sketch_files = sys.argv[6:]
+
+    # Merge the sketches from the files.
+    sketches = []
+    for file in sketch_files:
+        with open(file, encoding="utf8") as input_:
+            sketches.append({int(key): counter for key, counter
+                             in json.load(input_).items()})
+    merged = merge(sketches, sketch_size)
+
+    # Privatize the merged sketch.
+    if delta > 0:
+        private_merged = privatize_merged(merged, sketch_size, epsilon, delta)
+    else:
+        private_merged = purely_privatize_merged(merged, sketch_size, epsilon,
+                                                 universe_size)
+
+    # Output the merged sketch.
+    print("Merged        :", merged)
+    print("Private merged:", private_merged)
 
 
 def main():
@@ -171,64 +239,7 @@ def main():
         print("  Merge sketches with (epsilon, 0)-privacy:")
         print(f"    {sys.argv[0]} merge <sketch size> <epsilon> 0 "
               "<universe size> <sketch file> [<sketch file> ...]")
-
         return
-
-    def create_sketch():
-        k = int(sys.argv[1])
-        epsilon = float(sys.argv[2])
-        delta = float(sys.argv[3])
-        if delta > 0:
-            file = sys.argv[4]
-        else:
-            universe_size = int(sys.argv[4])
-            file = sys.argv[5]
-
-        with open(file, encoding="utf8") as stream:
-            stream = map(int, stream)
-            sketch, element_count, decrement_count = misra_gries(stream, k)
-
-        if delta > 0:
-            private_sketch = private_misra_gries(sketch, epsilon, delta)
-        else:
-            private_sketch = pure_private_misra_gries(
-                sketch, k, epsilon, element_count, decrement_count,
-                universe_size)
-
-        print("Sketch        :", sketch)
-        print("Private sketch:", private_sketch)
-
-        if ((delta > 0 and len(sys.argv) >= 6) or len(sys.argv) >= 7):
-            output_file = sys.argv[5] if delta > 0 else sys.argv[6]
-            with open(output_file, "w", encoding="utf8") as output:
-                json.dump(sketch, output)
-
-    def merge_sketches():
-        k = int(sys.argv[2])
-        epsilon = float(sys.argv[3])
-        delta = float(sys.argv[4])
-        if delta > 0:
-            sketch_files = sys.argv[5:]
-        else:
-            universe_size = int(sys.argv[5])
-            sketch_files = sys.argv[6:]
-
-        sketches = []
-        for file in sketch_files:
-            with open(file, encoding="utf8") as input_:
-                sketches.append({int(key): counter for key, counter
-                                 in json.load(input_).items()})
-
-        merged = merge(sketches, k)
-
-        if delta > 0:
-            private_merged = private_merge(merged, k, epsilon, delta)
-        else:
-            private_merged = pure_private_merge(merged, k, epsilon,
-                                                universe_size)
-
-        print("Merged        :", merged)
-        print("Private merged:", private_merged)
 
     if sys.argv[1] == "merge":
         merge_sketches()
